43bbbc22fc70223a0c4d1c91da5baa5dd3da7d3f
Mamat604 <mamat604@orange.fr>
2025-03-12 10:53:00 +0100

    Ajout du background pour les levels. Choix d'utiliser "background" plutôt qu'"Image" (à voir ce que ça donne)

--- code/src/levels/Level.java
+++ code/src/levels/Level.java
@@ -4,12 +4,20 @@ import src.model.Player;
 import src.model.Platform;
 import src.model.Enemy;
 import java.util.List;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.image.Image;
+
+
 import java.util.ArrayList;
 
 public abstract class Level {
     protected List<Platform> platforms;
     protected List<Enemy> enemies;
     protected Player player;
+    protected BackgroundImage backgroundImage;
 
     public Level(Player player) {
         this.player = player;
@@ -26,5 +34,14 @@ public abstract class Level {
 
     public List<Enemy> getEnemies() {
         return enemies;
+    }
+
+    public BackgroundImage getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    //Premier repeat : horizontal, Deuxième : Vertical
+    protected void setBackgroundImage(Image background) {
+        this.backgroundImage = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
     }
 }
