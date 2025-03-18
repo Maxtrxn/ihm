a292e44c0306eadf3a1acbb6cb24a0ed4185b074
Mamat604 <mamat604@orange.fr>
2025-03-18 15:51:37 +0100

    sol

--- code/src/levels/Level1.java
+++ code/src/levels/Level1.java
@@ -1,7 +1,10 @@
 package src.levels;
 
+import javafx.scene.image.Image;
 import src.model.Player;
+import src.model.Platform;
 import src.model.platforms.SteelPlatform;
+import src.model.platforms.GroundPlatform; // <-- On importe la nouvelle classe
 import src.model.Enemy;
 
 public class Level1 extends Level {
@@ -12,14 +15,36 @@ public class Level1 extends Level {
 
     @Override
     protected void initialize() {
+        // Arrière-plan
         setBackgroundImage("file:../textures/background temporaire1.png");
 
+        // ----------------------------------------------
+        // 1) Ajouter un sol répété sur la largeur du niveau
+        // ----------------------------------------------
+        double levelWidth = 3000;    // Largeur totale du niveau
+        double tileWidth  = 128;     // Largeur d'une tuile de sol
+        double floorY     = 580;     // Coordonnée Y du sol (bas ~600)
+
+        for (double x = 0; x < levelWidth; x += tileWidth) {
+            platforms.add(new GroundPlatform(
+                x,
+                floorY,
+                new Image("file:../textures/sol.png") // La texture du sol
+            ));
+        }
+
+        // ----------------------------------------------
+        // 2) Tes autres plateformes en acier
+        // ----------------------------------------------
         platforms.add(new SteelPlatform(200, 400));
         platforms.add(new SteelPlatform(600, 350));
         platforms.add(new SteelPlatform(1000, 400));
         platforms.add(new SteelPlatform(0, 580));
         platforms.add(new SteelPlatform(150, 450));
 
+        // ----------------------------------------------
+        // 3) Ennemis
+        // ----------------------------------------------
         enemies.add(new Enemy(650, 330, 50, 50, 2, 600, 800));
         enemies.add(new Enemy(1200, 330, 50, 50, 2, 1100, 1300));
     }
---
+++ code/src/model/platforms/GroundPlatform.java
@@ -0,0 +1,10 @@
+package src.model.platforms;
+
+import javafx.scene.image.Image;
+import src.model.Platform;
+
+public class GroundPlatform extends Platform {
+    public GroundPlatform(double x, double y, Image texture) {
+        super(x, y, texture);
+    }
+}
