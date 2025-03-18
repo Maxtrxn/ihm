f99a5703a9fde15eaca7709f0aa6bcd1d23eabc6
Mamat604 <mamat604@orange.fr>
2025-03-18 14:17:37 +0100

    ptn

--- code/src/model/platforms/SteelPlatform.java
+++ code/src/model/platforms/SteelPlatform.java
@@ -4,8 +4,11 @@ import javafx.scene.image.Image;
 import src.model.Platform;
 
 public class SteelPlatform extends Platform {
-
+    
     public SteelPlatform(double x, double y) {
-        super(x, y, new Image("file:../textures/plateforme_acier_1.png"));
+        super(x, y, new Image("file:src/textures/plateforme_acier_1.png"), /* other necessary arguments */);
+        if (this.texture.isError()) {
+            System.out.println("Error loading SteelPlatform texture.");
+        }
     }
 }
