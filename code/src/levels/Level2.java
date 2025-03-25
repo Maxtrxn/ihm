739d069d50c87b1f47d5f55b569891e4fbba958e
Mamat604 <mamat604@orange.fr>
2025-03-25 14:43:06 +0100

    tests

--- code/src/levels/Level2.java
+++ code/src/levels/Level2.java
@@ -15,13 +15,24 @@ public class Level2 extends Level {
     protected void initialize() {
         setBackgroundImage("file:../textures/background_level2.png");
 
+        // Plateformes existantes
         platforms.add(new FragilePlatform(100, 300)); // Première plateforme cassée
         platforms.add(new CopperPlatform(400, 250));
         platforms.add(new CopperPlatform(700, 300));
-        platforms.add(new CopperPlatform(0, 580));
         platforms.add(new CopperPlatform(200, 350));
         platforms.add(new CopperPlatform(400, 350));
 
+        // Ajout d'un sol dur sur toute la largeur du niveau
+        // Supposons que la largeur logique du niveau est de 3000 pixels
+        // et que chaque CopperPlatform a une largeur par défaut de 96 pixels.
+        int levelWidth = 3000;
+        int groundY = 580;
+        int platformWidth = 96; // ajustez si besoin selon la définition de CopperPlatform
+        for (int x = 0; x < levelWidth; x += platformWidth) {
+            platforms.add(new CopperPlatform(x, groundY));
+        }
+
+        // Ennemis
         enemies.add(new Enemy(450, 230, 50, 50, 2, 400, 600));
         enemies.add(new Enemy(800, 230, 50, 50, 2, 700, 900));
     }
